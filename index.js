3f386a526d2230c6cd3c114042642a8522b7b063
SM RIYAD <smriyad0185@gmail.com>
2024-03-21 15:15:59 +0600

    delete user api is done

--- index.js
+++ index.js
@@ -152,6 +152,15 @@ app.patch('/updateuser/:id', async (req, res) => {
   res.send(result);
 })
 
+///delete a review api
+
+app.delete("/deleteuser/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
+  console.log(result);
+  res.send(result);
+});
 
 
 
