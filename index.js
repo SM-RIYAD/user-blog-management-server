9ad13ba2cbbdcde3dfba8874f7241ca4b1854569
SM RIYAD <smriyad0185@gmail.com>
2024-03-21 23:25:59 +0600

    added vercel

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://user-blog-management.surge.sh" 
+}));
+
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173","https://user-blog-management.surge.sh/"],
+//     credentials: true,
+//   })
+// );
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wacbf1n.mongodb.net/?retryWrites=true&w=majority`;
